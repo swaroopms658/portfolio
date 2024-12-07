a8b61a53b2890515d0eda76e459e4a03849d8d0b
swaroop <swaroopms658@gmail.com>
2024-12-07 15:18:52 +0530

    updateion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
             </li>
 
             <li>
-              <a href="/Swaroop_Resume.pdf" download="">
+              <a href="/Swaroop_resume.pdf" download="">
                 Download Resume
               </a>
             </li>
